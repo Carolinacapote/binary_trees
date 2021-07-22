fc803dbd50fe1bfafc4804e4deef0352de591534
Carolinacapote <carolina.capote28@gmail.com>
2021-07-22 15:50:52 +0000

    [fix] (task 12->betty style)

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,7 +10,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->right == NULL && tree->left == NULL)
-		return ( 1);
+		return (1);
 	return (binary_tree_leaves(tree->right) + binary_tree_leaves(tree->left));
 }
 
